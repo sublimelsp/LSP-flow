19a1e70e6d13f5a2b1a8ed4e9724d6f4923eefe2
Zach Schneider <zach@schneider.dev>
2021-06-26 05:09:44 -0500

    Recursively search up directory structure to find .flowconfig (#6)

--- plugin.py
+++ plugin.py
@@ -5,8 +5,7 @@ from LSP.plugin.core.typing import Optional, List
 import sublime
 
 
-from os.path import join
-from os.path import exists
+from os.path import dirname, exists, join
 
 
 def _flow_bin_path(path: str) -> str:
@@ -18,22 +17,38 @@ def _flow_config_path(path: str) -> str:
     return join(path, ".flowconfig")
 
 
+def _find_flow_root_path(directory: str) -> Optional[str]:
+    if exists(_flow_config_path(directory)):
+        return directory
+    elif directory == dirname(directory):
+        return None
+    else:
+        return _find_flow_root_path(dirname(directory))
+
+
 class Flow(AbstractPlugin):
     @classmethod
     def name(cls) -> str:
         return cls.__name__.lower()
 
     @classmethod
-    def can_start(cls, _: sublime.Window, __: sublime.View,
-        folders: List[WorkspaceFolder], configuration: ClientConfig
+    def can_start(
+        cls,
+        _: sublime.Window,
+        initiating_view: sublime.View,
+        folders: List[WorkspaceFolder],
+        configuration: ClientConfig,
     ) -> Optional[str]:
-        if not folders:
-            return "need a folder"
-        path = folders[0].path
-        if not exists(_flow_config_path(path)):
-            return "no .flowconfig present in {}".format(path)
+        path = _find_flow_root_path(dirname(initiating_view.file_name()))
+
+        if not path:
+            return "no .flowconfig found in project or parent directories"
+
         binpath = _flow_bin_path(path)
-        if not exists(_flow_bin_path(path)):
+        if not exists(binpath):
             return "no flow binary found (tried: {})".format(binpath)
+
+        folders.clear()
+        folders.append(WorkspaceFolder.from_path(path))
         configuration.command = [binpath, "lsp"]
         return None
